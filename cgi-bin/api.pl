ac3287245eee580eb0038c4504bf2f44aa40736c
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-03 08:18:27 -0600

    actu

--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -24,6 +24,15 @@ sub guardar_db {
     close $fh;
 }
 
+# FUNCIÓN DE LÓGICA: Clasifica según la edad
+sub clasificar_rango {
+    my ($edad) = @_;
+    if ($edad < 0) { return "Invalido"; }
+    if ($edad < 18) { return "Joven"; }
+    if ($edad < 100) { return "Adulto"; }
+    return "Legendario (No Humano)";
+}
+
 my $db = leer_db();
 my $metodo = $cgi->request_method();
 
@@ -38,25 +47,40 @@ if ($metodo eq 'GET') {
 } 
 elsif ($metodo eq 'POST') {
     my $nuevo = decode_json($cgi->param('POSTDATA'));
+    
+    # Validar edad máxima
+    if ($nuevo->{edad} > 150) {
+        print encode_json({ error => "Edad fuera de rango humano" });
+        exit;
+    }
+
+    # Asignar Rango y ID
+    $nuevo->{rango} = clasificar_rango($nuevo->{edad});
     my $max_id = 0;
     foreach my $p (@{$db->{data}}) { $max_id = $p->{id} if $p->{id} > $max_id; }
     $nuevo->{id} = $max_id + 1;
+    
     push @{$db->{data}}, $nuevo;
     guardar_db($db);
-    print encode_json({ status => "Creado", id => $nuevo->{id} });
+    print encode_json({ status => "Creado", id => $nuevo->{id}, rango => $nuevo->{rango} });
 } 
 elsif ($metodo eq 'PATCH') {
     my $update = decode_json($cgi->param('POSTDATA'));
+    my $hecho = 0;
     foreach my $p (@{$db->{data}}) {
         if ($p->{id} == $update->{id}) {
             $p->{nombre} = $update->{nombre} if $update->{nombre};
             $p->{universo} = $update->{universo} if $update->{universo};
             $p->{nivel_poder} = $update->{nivel_poder} if $update->{nivel_poder};
-            last;
+            if ($update->{edad}) {
+                $p->{edad} = $update->{edad};
+                $p->{rango} = clasificar_rango($update->{edad});
+            }
+            $hecho = 1; last;
         }
     }
-    guardar_db($db);
-    print encode_json({ status => "Ok" });
+    guardar_db($db) if $hecho;
+    print encode_json({ status => $hecho ? "Ok" : "No encontrado" });
 } 
 elsif ($metodo eq 'DELETE') {
     my $id = $cgi->param('id');
