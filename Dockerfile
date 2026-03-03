d5e60860073a97d9d33716edc0ac4df3cedfa671
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-02 18:58:24 -0600

    actu

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,21 @@
 FROM debian:bullseye
 
-RUN apt-get update && \
-    apt-get install -y apache2 perl libcgi-pm-perl libjson-perl && \
-    apt-get clean
+RUN apt-get update && apt-get install -y apache2 perl libcgi-pm-perl libjson-perl && apt-get clean
+RUN a2enmod cgid headers
 
-RUN a2enmod cgid
-RUN rm -f /var/www/html/index.html
-
-# Copiamos archivos al servidor
+# Limpiar y copiar
+RUN rm -rf /var/www/html/*
 COPY index.html /var/www/html/index.html
 COPY swagger.html /var/www/html/swagger.html
 COPY swagger.json /var/www/html/swagger.json
 COPY ./cgi-bin/ /usr/lib/cgi-bin/
 
-# Permisos de ejecución y escritura
+# Permisos
 RUN chmod +x /usr/lib/cgi-bin/api.pl
-RUN chmod 777 /usr/lib/cgi-bin
+RUN chmod -R 777 /usr/lib/cgi-bin
 RUN chmod 666 /usr/lib/cgi-bin/datos.json
 
-# Asegurar que index.html sea la principal
+# Configuración Apache
 RUN echo "DirectoryIndex index.html" >> /etc/apache2/apache2.conf
 
 CMD ["apachectl", "-D", "FOREGROUND"]
--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -7,51 +7,44 @@ use JSON;
 my $cgi = CGI->new;
 my $archivo = '/usr/lib/cgi-bin/datos.json';
 
-# Cabecera para JSON
 print $cgi->header('application/json; charset=UTF-8');
 
-sub leer_json {
-    if (!-e $archivo || -s $archivo == 0) { return { status => "ok", data => [] }; }
-    open my $fh, '<', $archivo or return { status => "ok", data => [] };
-    my $content = do { local $/; <$fh> };
+# Leer base de datos
+sub leer {
+    if (!-e $archivo) { return { data => [] }; }
+    open my $fh, '<', $archivo or return { data => [] };
+    my $txt = do { local $/; <$fh> };
     close $fh;
-    return decode_json($content);
+    return $txt ? decode_json($txt) : { data => [] };
 }
 
-sub guardar_json {
-    my ($datos) = @_;
-    open my $fh, '>', $archivo or die "Error: $!";
-    print $fh encode_json($datos);
-    close $fh;
-}
-
-my $datos = leer_json();
+my $db = leer();
 my $metodo = $cgi->request_method();
 
-if ($metodo eq 'POST') {
-    # CREAR
-    my $json_input = $cgi->param('POSTDATA');
-    my $nuevo = decode_json($json_input);
-    push @{$datos->{data}}, $nuevo;
-} 
-elsif ($metodo eq 'PATCH') {
-    # ACTUALIZAR
-    my $json_input = $cgi->param('POSTDATA');
-    my $update = decode_json($json_input);
-    foreach my $pj (@{$datos->{data}}) {
-        if ($pj->{nombre} eq $update->{nombre}) {
-            $pj->{universo} = $update->{universo} if $update->{universo};
-            $pj->{nivel_poder} = $update->{nivel_poder} if $update->{nivel_poder};
-            last;
+if ($metodo eq 'POST' || $metodo eq 'PATCH') {
+    my $raw = $cgi->param('POSTDATA');
+    if ($raw) {
+        my $item = decode_json($raw);
+        if ($metodo eq 'POST') {
+            push @{$db->{data}}, $item;
+        } else {
+            foreach my $p (@{$db->{data}}) {
+                if ($p->{nombre} eq $item->{nombre}) {
+                    $p->{universo} = $item->{universo} if $item->{universo};
+                    $p->{nivel_poder} = $item->{nivel_poder} if $item->{nivel_poder};
+                }
+            }
         }
     }
-} 
-elsif ($metodo eq 'DELETE') {
-    # ELIMINAR
-    my $nombre_a_borrar = $cgi->param('nombre');
-    my @filtrado = grep { $_->{nombre} ne $nombre_a_borrar } @{$datos->{data}};
-    $datos->{data} = \@filtrado;
+} elsif ($metodo eq 'DELETE') {
+    my $nom = $cgi->param('nombre');
+    my @new = grep { $_->{nombre} ne $nom } @{$db->{data}};
+    $db->{data} = \@new;
 }
 
-guardar_json($datos);
-print encode_json($datos); # Retorna la lista actualizada
+# Guardar
+open my $fh, '>', $archivo;
+print $fh encode_json($db);
+close $fh;
+
+print encode_json($db);
