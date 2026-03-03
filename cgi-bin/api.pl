b01837df14ee7b402a7c32ec79cba85cb7a040f6
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-03 08:28:02 -0600

    actu

--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -6,6 +6,7 @@ use JSON;
 
 my $cgi = CGI->new;
 my $archivo = '/usr/lib/cgi-bin/datos.json';
+my $PASSWORD_MAESTRA = "admin123"; # Esta es la contraseña para editar
 
 print $cgi->header('application/json; charset=UTF-8');
 
@@ -24,37 +25,35 @@ sub guardar_db {
     close $fh;
 }
 
-# FUNCIÓN DE LÓGICA: Clasifica según la edad
 sub clasificar_rango {
     my ($edad) = @_;
-    if ($edad < 0) { return "Invalido"; }
-    if ($edad < 18) { return "Joven"; }
-    if ($edad < 100) { return "Adulto"; }
-    return "Legendario (No Humano)";
+    return $edad < 18 ? "Joven" : $edad < 100 ? "Adulto" : "Legendario";
 }
 
 my $db = leer_db();
 my $metodo = $cgi->request_method();
 
-if ($metodo eq 'GET') {
-    my $id = $cgi->param('id');
-    if ($id) {
-        my ($p) = grep { $_->{id} == $id } @{$db->{data}};
-        print encode_json($p || { error => "No existe" });
-    } else {
-        print encode_json($db);
+# --- SEGURIDAD: Validar contraseña en métodos de escritura ---
+if ($metodo ne 'GET') {
+    my $auth = $cgi->http('HTTP_X_AUTH_TOKEN') || ""; 
+    if ($auth ne $PASSWORD_MAESTRA) {
+        print encode_json({ error => "No tienes permiso (Login requerido)" });
+        exit;
     }
+}
+
+if ($metodo eq 'GET') {
+    print encode_json($db);
 } 
 elsif ($metodo eq 'POST') {
     my $nuevo = decode_json($cgi->param('POSTDATA'));
     
-    # Validar edad máxima
-    if ($nuevo->{edad} > 150) {
-        print encode_json({ error => "Edad fuera de rango humano" });
+    # VALIDACIÓN DE 3 CIFRAS
+    if ($nuevo->{edad} > 999 || $nuevo->{nivel_poder} > 999) {
+        print encode_json({ error => "Error: No se permiten valores de más de 3 cifras" });
         exit;
     }
 
-    # Asignar Rango y ID
     $nuevo->{rango} = clasificar_rango($nuevo->{edad});
     my $max_id = 0;
     foreach my $p (@{$db->{data}}) { $max_id = $p->{id} if $p->{id} > $max_id; }
@@ -62,25 +61,24 @@ elsif ($metodo eq 'POST') {
     
     push @{$db->{data}}, $nuevo;
     guardar_db($db);
-    print encode_json({ status => "Creado", id => $nuevo->{id}, rango => $nuevo->{rango} });
+    print encode_json({ status => "Creado", id => $nuevo->{id} });
 } 
 elsif ($metodo eq 'PATCH') {
     my $update = decode_json($cgi->param('POSTDATA'));
-    my $hecho = 0;
     foreach my $p (@{$db->{data}}) {
         if ($p->{id} == $update->{id}) {
             $p->{nombre} = $update->{nombre} if $update->{nombre};
-            $p->{universo} = $update->{universo} if $update->{universo};
-            $p->{nivel_poder} = $update->{nivel_poder} if $update->{nivel_poder};
             if ($update->{edad}) {
+                if($update->{edad} > 999) { print encode_json({error=>"Max 3 cifras"}); exit; }
                 $p->{edad} = $update->{edad};
                 $p->{rango} = clasificar_rango($update->{edad});
             }
-            $hecho = 1; last;
+            $p->{nivel_poder} = $update->{nivel_poder} if $update->{nivel_poder};
+            last;
         }
     }
-    guardar_db($db) if $hecho;
-    print encode_json({ status => $hecho ? "Ok" : "No encontrado" });
+    guardar_db($db);
+    print encode_json({ status => "Ok" });
 } 
 elsif ($metodo eq 'DELETE') {
     my $id = $cgi->param('id');
