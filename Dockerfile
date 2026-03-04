8abd9c1e50429294a9328181c7fc51324848a032
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-03 18:52:51 -0600

    actu

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,21 @@
 FROM httpd:2.4
 
-# Dependencias para Perl y JSON
+# Librerías
 RUN apt-get update && apt-get install -y perl libjson-perl libfile-slurp-perl && rm -rf /var/lib/apt/lists/*
 
-# Configuración de Apache para scripts CGI
+# Configuración CGI
 RUN sed -i 's/#LoadModule cgi_module/LoadModule cgi_module/' /usr/local/apache2/conf/httpd.conf
 RUN echo "AddHandler cgi-script .pl" >> /usr/local/apache2/conf/httpd.conf
 RUN sed -i 's/Options Indexes FollowSymLinks/Options Indexes FollowSymLinks ExecCGI/' /usr/local/apache2/conf/httpd.conf
 
-# Copiar archivos del proyecto Erick
+# Copia
 COPY ./index.html /usr/local/apache2/htdocs/
 COPY ./swagger.html /usr/local/apache2/htdocs/
 COPY ./cgi-bin/ /usr/local/apache2/cgi-bin/
 
-# PERMISOS DE ESCRITURA (Vital para que el POST guarde datos)
-RUN touch /usr/local/apache2/cgi-bin/datos.json
-RUN chmod 777 /usr/local/apache2/cgi-bin/datos.json
-RUN chmod 777 /usr/local/apache2/cgi-bin/
-RUN chmod +x /usr/local/apache2/cgi-bin/api.pl
+# PERMISOS TOTALES (Solución al error de guardado)
+RUN echo "[]" > /usr/local/apache2/cgi-bin/datos.json
+RUN chmod -R 777 /usr/local/apache2/cgi-bin/
+RUN chown -R www-data:www-data /usr/local/apache2/cgi-bin/
 
 EXPOSE 80
--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -7,7 +7,7 @@ use File::Slurp;
 
 my $cgi = CGI->new;
 my $metodo = $cgi->request_method();
-my $ruta_db = "datos.json";
+my $ruta_db = "/usr/local/apache2/cgi-bin/datos.json";
 my $token_maestro = "admin123";
 my $token_cliente = $cgi->http('HTTP_X_AUTH_TOKEN') || $cgi->http('X-Auth-Token') || "";
 
@@ -28,56 +28,34 @@ if (-e $ruta_db) {
     $personajes = decode_json($contenido || '[]');
 }
 
+# Seguridad para métodos que modifican datos
 if ($metodo ne 'GET' && $token_cliente ne $token_maestro) {
     print encode_json({ error => "Token invalido" });
     exit;
 }
 
+# --- ENDPOINTS ---
+
 if ($metodo eq 'GET') {
     my $id_p = $cgi->param('id');
-    if ($id_p) {
-        my ($p) = grep { $_->{id} == $id_p } @$personajes;
-        print encode_json($p || { error => "No encontrado" });
-    } else {
-        print encode_json($personajes);
-    }
+    my @res = @$personajes;
+    if ($id_p) { @res = grep { $_->{id} == $id_p } @res; }
+    print encode_json(\@res);
 }
 elsif ($metodo eq 'POST') {
-    # SOLUCIÓN AL ERROR DE GUARDADO:
-    # Capturamos el JSON crudo del cuerpo de la petición
-    my $json_raw = $cgi->param('POSTDATA') || $cgi->param('keywords') || "{}";
-    
-    my $nuevo;
-    eval { $nuevo = decode_json($json_raw); };
-    
-    if ($@ || !$nuevo->{nombre}) {
-        print encode_json({ error => "Datos invalidos o vacios", detalle => $@ });
-        exit;
-    }
-
-    # Generar ID basado en el último elemento real
-    my $ultimo_id = 0;
-    if (scalar @$personajes > 0) {
-        $ultimo_id = $personajes->[-1]->{id};
-    }
-    $nuevo->{id} = $ultimo_id + 1;
-
-    # Clasificar Rango
-    my $e = $nuevo->{edad} || 0;
-    $nuevo->{rango} = $e >= 100 ? "Legendario" : ($e >= 18 ? "Adulto" : "Joven");
-
+    my $json_raw = $cgi->param('POSTDATA') || "{}";
+    my $nuevo = decode_json($json_raw);
+    $nuevo->{id} = (@$personajes > 0) ? $personajes->[-1]->{id} + 1 : 1;
+    my $edad = $nuevo->{edad} || 0;
+    $nuevo->{rango} = $edad >= 100 ? "Legendario" : ($edad >= 18 ? "Adulto" : "Joven");
     push @$personajes, $nuevo;
-    
-    # Escribir físicamente en el archivo
     write_file($ruta_db, {binmode => ':utf8'}, encode_json($personajes));
-    
-    print encode_json({ status => "Creado", id => $nuevo->{id}, guerrero => $nuevo });
+    print encode_json({ status => "Creado", id => $nuevo->{id} });
 }
 elsif ($metodo eq 'PUT') {
     my $json_raw = $cgi->param('POSTDATA') || "{}";
     my $edit = decode_json($json_raw);
     my $exito = 0;
-
     for (my $i = 0; $i < @$personajes; $i++) {
         if ($personajes->[$i]->{id} == $edit->{id}) {
             $edit->{rango} = ($edit->{edad} >= 100) ? "Legendario" : (($edit->{edad} >= 18) ? "Adulto" : "Joven");
