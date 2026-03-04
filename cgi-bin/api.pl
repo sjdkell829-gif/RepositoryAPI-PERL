e69bcc4b092a1319f8d383273de0a5ab3206cba8
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-03 18:41:56 -0600

    actu

--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -9,11 +9,8 @@ my $cgi = CGI->new;
 my $metodo = $cgi->request_method();
 my $ruta_db = "datos.json";
 my $token_maestro = "admin123";
-
-# Captura de token (compatible con Render y Swagger)
 my $token_cliente = $cgi->http('HTTP_X_AUTH_TOKEN') || $cgi->http('X-Auth-Token') || "";
 
-# Encabezado JSON obligatorio para que Swagger no falle
 print $cgi->header(
     -type => 'application/json',
     -charset => 'utf-8',
@@ -24,94 +21,81 @@ print $cgi->header(
 
 exit if $metodo eq 'OPTIONS';
 
-sub leer_db {
-    if (-e $ruta_db) {
-        my $contenido = read_file($ruta_db);
-        return decode_json($contenido || '[]');
-    }
-    return [];
+# Leer Base de Datos
+my $personajes = [];
+if (-e $ruta_db) {
+    my $contenido = read_file($ruta_db);
+    $personajes = decode_json($contenido || '[]');
 }
 
-sub guardar_db {
-    my ($data) = @_;
-    write_file($ruta_db, {binmode => ':utf8'}, encode_json($data));
-}
-
-sub clasificar_rango {
-    my $edad = shift || 0;
-    return $edad >= 100 ? "Legendario" : ($edad >= 18 ? "Adulto" : "Joven");
-}
-
-my $personajes = leer_db();
-
-# Seguridad: GET es público, lo demás requiere token
 if ($metodo ne 'GET' && $token_cliente ne $token_maestro) {
-    print encode_json({ error => "Acceso denegado. Token invalido.", recibido => $token_cliente });
+    print encode_json({ error => "Token invalido" });
     exit;
 }
 
-# --- ENDPOINTS (GET, POST, PUT, DELETE) ---
-
 if ($metodo eq 'GET') {
     my $id_p = $cgi->param('id');
-    my $uni_p = $cgi->param('universo');
-    my $pow_p = $cgi->param('poder_min');
-
-    my @res = @$personajes;
-
-    # Filtrar por ID individual
     if ($id_p) {
-        @res = grep { $_->{id} == $id_p } @res;
-    }
-    # Filtrar por Universo
-    if ($uni_p) {
-        @res = grep { lc($_->{universo}) eq lc($uni_p) } @res;
-    }
-    # Filtrar por Poder Mínimo
-    if ($pow_p) {
-        @res = grep { $_->{nivel_poder} >= $pow_p } @res;
-    }
-
-    # Si se buscó por ID y no hay nada, avisar
-    if ($id_p && !@res) {
-        print encode_json({ error => "Guerrero con ID $id_p no encontrado" });
+        my ($p) = grep { $_->{id} == $id_p } @$personajes;
+        print encode_json($p || { error => "No encontrado" });
     } else {
-        print encode_json(\@res);
+        print encode_json($personajes);
     }
 }
 elsif ($metodo eq 'POST') {
-    my $json = decode_json($cgi->param('POSTDATA') || '{}');
-    $json->{id} = (@$personajes > 0) ? $personajes->[-1]->{id} + 1 : 1;
-    $json->{rango} = clasificar_rango($json->{edad});
-    push @$personajes, $json;
-    guardar_db($personajes);
-    print encode_json({ status => "Creado con exito", id => $json->{id}, guerrero => $json });
+    # SOLUCIÓN AL ERROR DE GUARDADO:
+    # Capturamos el JSON crudo del cuerpo de la petición
+    my $json_raw = $cgi->param('POSTDATA') || $cgi->param('keywords') || "{}";
+    
+    my $nuevo;
+    eval { $nuevo = decode_json($json_raw); };
+    
+    if ($@ || !$nuevo->{nombre}) {
+        print encode_json({ error => "Datos invalidos o vacios", detalle => $@ });
+        exit;
+    }
+
+    # Generar ID basado en el último elemento real
+    my $ultimo_id = 0;
+    if (scalar @$personajes > 0) {
+        $ultimo_id = $personajes->[-1]->{id};
+    }
+    $nuevo->{id} = $ultimo_id + 1;
+
+    # Clasificar Rango
+    my $e = $nuevo->{edad} || 0;
+    $nuevo->{rango} = $e >= 100 ? "Legendario" : ($e >= 18 ? "Adulto" : "Joven");
+
+    push @$personajes, $nuevo;
+    
+    # Escribir físicamente en el archivo
+    write_file($ruta_db, {binmode => ':utf8'}, encode_json($personajes));
+    
+    print encode_json({ status => "Creado", id => $nuevo->{id}, guerrero => $nuevo });
 }
 elsif ($metodo eq 'PUT') {
-    my $edit = decode_json($cgi->param('POSTDATA') || '{}');
+    my $json_raw = $cgi->param('POSTDATA') || "{}";
+    my $edit = decode_json($json_raw);
     my $exito = 0;
+
     for (my $i = 0; $i < @$personajes; $i++) {
         if ($personajes->[$i]->{id} == $edit->{id}) {
-            $edit->{rango} = clasificar_rango($edit->{edad});
+            $edit->{rango} = ($edit->{edad} >= 100) ? "Legendario" : (($edit->{edad} >= 18) ? "Adulto" : "Joven");
             $personajes->[$i] = $edit;
             $exito = 1;
             last;
         }
     }
     if ($exito) {
-        guardar_db($personajes);
-        print encode_json({ status => "Actualizado correctamente", id => $edit->{id} });
+        write_file($ruta_db, {binmode => ':utf8'}, encode_json($personajes));
+        print encode_json({ status => "Actualizado" });
     } else {
-        print encode_json({ error => "No se pudo actualizar. ID no encontrado." });
+        print encode_json({ error => "ID no encontrado" });
     }
 }
 elsif ($metodo eq 'DELETE') {
     my $id_b = $cgi->param('id');
     my @filt = grep { $_->{id} != $id_b } @$personajes;
-    if (scalar @filt == scalar @$personajes) {
-        print encode_json({ error => "ID no encontrado para eliminar" });
-    } else {
-        guardar_db(\@filt);
-        print encode_json({ status => "Guerrero eliminado satisfactoriamente", id => $id_b });
-    }
+    write_file($ruta_db, {binmode => ':utf8'}, encode_json(\@filt));
+    print encode_json({ status => "Eliminado" });
 }
