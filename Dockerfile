ee541907d9915e9de5fb8b88096eac138e7e7cba
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-03 18:32:23 -0600

    actu

--- Dockerfile
+++ Dockerfile
@@ -1,40 +1,22 @@
-# 1. Usamos la imagen oficial de Apache (servidor web)
 FROM httpd:2.4
 
-# 2. Instalamos Perl y las librerías necesarias para manejar JSON y archivos
-# libjson-perl: para que Perl entienda el formato JSON
-# libfile-slurp-perl: para leer y escribir archivos de forma rápida
-RUN apt-get update && apt-get install -y \
-    perl \
-    libjson-perl \
-    libfile-slurp-perl \
-    && rm -rf /var/lib/apt/lists/*
+# Dependencias para Perl y JSON
+RUN apt-get update && apt-get install -y perl libjson-perl libfile-slurp-perl && rm -rf /var/lib/apt/lists/*
 
-# 3. Configuramos Apache para que permita ejecutar archivos .pl (CGI)
-# Activamos el módulo CGI
+# Configuración de Apache para scripts CGI
 RUN sed -i 's/#LoadModule cgi_module/LoadModule cgi_module/' /usr/local/apache2/conf/httpd.conf
-
-# Le decimos que los archivos .pl se traten como scripts
 RUN echo "AddHandler cgi-script .pl" >> /usr/local/apache2/conf/httpd.conf
-
-# Damos permiso de ejecución (ExecCGI) en la carpeta de htdocs y cgi-bin
 RUN sed -i 's/Options Indexes FollowSymLinks/Options Indexes FollowSymLinks ExecCGI/' /usr/local/apache2/conf/httpd.conf
 
-# 4. Copiamos tus archivos al contenedor
-# Los HTML van a la carpeta pública htdocs
+# Copiar archivos del proyecto Erick
 COPY ./index.html /usr/local/apache2/htdocs/
 COPY ./swagger.html /usr/local/apache2/htdocs/
-
-# El script de Perl va a la carpeta especial cgi-bin
 COPY ./cgi-bin/ /usr/local/apache2/cgi-bin/
 
-# 5. Permisos de seguridad y escritura
-# Hacemos que el script de Perl sea ejecutable
+# PERMISOS DE ESCRITURA (Vital para que el POST guarde datos)
+RUN touch /usr/local/apache2/cgi-bin/datos.json
+RUN chmod 777 /usr/local/apache2/cgi-bin/datos.json
+RUN chmod 777 /usr/local/apache2/cgi-bin/
 RUN chmod +x /usr/local/apache2/cgi-bin/api.pl
 
-# Creamos el archivo de base de datos vacío y le damos permisos totales (777)
-# Esto es vital para que el servidor pueda guardar los cambios del POST y PUT
-RUN touch /usr/local/apache2/cgi-bin/datos.json && chmod 777 /usr/local/apache2/cgi-bin/datos.json
-
-# 6. Exponemos el puerto 80 (estándar web)
 EXPOSE 80
--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -7,21 +7,22 @@ use File::Slurp;
 
 my $cgi = CGI->new;
 my $metodo = $cgi->request_method();
-
-# AJUSTE CRÍTICO: Se usa HTTP_X_AUTH_TOKEN para máxima compatibilidad en Render/Linux
-my $token_cliente = $cgi->http('HTTP_X_AUTH_TOKEN') || $cgi->http('X-Auth-Token') || "";
-my $token_maestro = "admin123";
 my $ruta_db = "datos.json";
+my $token_maestro = "admin123";
 
-# Encabezados CORS y JSON
+# Captura de token (compatible con Render y Swagger)
+my $token_cliente = $cgi->http('HTTP_X_AUTH_TOKEN') || $cgi->http('X-Auth-Token') || "";
+
+# Encabezado JSON obligatorio para que Swagger no falle
 print $cgi->header(
     -type => 'application/json',
+    -charset => 'utf-8',
     -access_control_allow_origin => '*',
     -access_control_allow_methods => 'GET, POST, PUT, DELETE, OPTIONS',
     -access_control_allow_headers => 'Content-Type, X-Auth-Token'
 );
 
-if ($metodo eq 'OPTIONS') { exit; }
+exit if $metodo eq 'OPTIONS';
 
 sub leer_db {
     if (-e $ruta_db) {
@@ -33,7 +34,7 @@ sub leer_db {
 
 sub guardar_db {
     my ($data) = @_;
-    write_file($ruta_db, encode_json($data));
+    write_file($ruta_db, {binmode => ':utf8'}, encode_json($data));
 }
 
 sub clasificar_rango {
@@ -41,54 +42,76 @@ sub clasificar_rango {
     return $edad >= 100 ? "Legendario" : ($edad >= 18 ? "Adulto" : "Joven");
 }
 
-# Validación de Seguridad
+my $personajes = leer_db();
+
+# Seguridad: GET es público, lo demás requiere token
 if ($metodo ne 'GET' && $token_cliente ne $token_maestro) {
-    print encode_json({ error => "Token invalido o ausente", recibido => $token_cliente });
+    print encode_json({ error => "Acceso denegado. Token invalido.", recibido => $token_cliente });
     exit;
 }
 
-my $personajes = leer_db();
+# --- ENDPOINTS (GET, POST, PUT, DELETE) ---
 
 if ($metodo eq 'GET') {
-    my $id_buscado = $cgi->param('id');
-    if ($id_buscado) {
-        my ($p) = grep { $_->{id} == $id_buscado } @$personajes;
-        print encode_json($p || { error => "ID no encontrado" });
+    my $id_p = $cgi->param('id');
+    my $uni_p = $cgi->param('universo');
+    my $pow_p = $cgi->param('poder_min');
+
+    my @res = @$personajes;
+
+    # Filtrar por ID individual
+    if ($id_p) {
+        @res = grep { $_->{id} == $id_p } @res;
+    }
+    # Filtrar por Universo
+    if ($uni_p) {
+        @res = grep { lc($_->{universo}) eq lc($uni_p) } @res;
+    }
+    # Filtrar por Poder Mínimo
+    if ($pow_p) {
+        @res = grep { $_->{nivel_poder} >= $pow_p } @res;
+    }
+
+    # Si se buscó por ID y no hay nada, avisar
+    if ($id_p && !@res) {
+        print encode_json({ error => "Guerrero con ID $id_p no encontrado" });
     } else {
-        print encode_json($personajes);
+        print encode_json(\@res);
     }
 }
 elsif ($metodo eq 'POST') {
-    my $json_texto = $cgi->param('POSTDATA') || '{}';
-    my $nuevo = decode_json($json_texto);
-    $nuevo->{id} = (@$personajes > 0) ? $personajes->[-1]->{id} + 1 : 1;
-    $nuevo->{rango} = clasificar_rango($nuevo->{edad});
-    push @$personajes, $nuevo;
+    my $json = decode_json($cgi->param('POSTDATA') || '{}');
+    $json->{id} = (@$personajes > 0) ? $personajes->[-1]->{id} + 1 : 1;
+    $json->{rango} = clasificar_rango($json->{edad});
+    push @$personajes, $json;
     guardar_db($personajes);
-    print encode_json({ status => "Creado", id => $nuevo->{id} });
+    print encode_json({ status => "Creado con exito", id => $json->{id}, guerrero => $json });
 }
 elsif ($metodo eq 'PUT') {
-    my $json_texto = $cgi->param('POSTDATA') || '{}';
-    my $editado = decode_json($json_texto);
-    my $encontrado = 0;
+    my $edit = decode_json($cgi->param('POSTDATA') || '{}');
+    my $exito = 0;
     for (my $i = 0; $i < @$personajes; $i++) {
-        if ($personajes->[$i]->{id} == $editado->{id}) {
-            $editado->{rango} = clasificar_rango($editado->{edad});
-            $personajes->[$i] = $editado;
-            $encontrado = 1;
+        if ($personajes->[$i]->{id} == $edit->{id}) {
+            $edit->{rango} = clasificar_rango($edit->{edad});
+            $personajes->[$i] = $edit;
+            $exito = 1;
             last;
         }
     }
-    if ($encontrado) {
+    if ($exito) {
         guardar_db($personajes);
-        print encode_json({ status => "Actualizado" });
+        print encode_json({ status => "Actualizado correctamente", id => $edit->{id} });
     } else {
-        print encode_json({ error => "ID no encontrado" });
+        print encode_json({ error => "No se pudo actualizar. ID no encontrado." });
     }
 }
 elsif ($metodo eq 'DELETE') {
-    my $id_borrar = $cgi->param('id');
-    my @filtrados = grep { $_->{id} != $id_borrar } @$personajes;
-    guardar_db(\@filtrados);
-    print encode_json({ status => "Eliminado" });
+    my $id_b = $cgi->param('id');
+    my @filt = grep { $_->{id} != $id_b } @$personajes;
+    if (scalar @filt == scalar @$personajes) {
+        print encode_json({ error => "ID no encontrado para eliminar" });
+    } else {
+        guardar_db(\@filt);
+        print encode_json({ status => "Guerrero eliminado satisfactoriamente", id => $id_b });
+    }
 }
