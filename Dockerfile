06c66b5b5a11bf7f0b3b97170e2f010375af0d75
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-04 09:17:29 -0600

    actu

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,19 @@
 FROM httpd:2.4
 
-# Instalar Perl y JSON
+# Instalar Perl y librerías JSON
 RUN apt-get update && apt-get install -y perl libjson-perl libfile-slurp-perl && rm -rf /var/lib/apt/lists/*
 
-# Configurar Apache para CGI
+# Configurar Apache para que ejecute archivos .pl como CGI
 RUN sed -i 's/#LoadModule cgi_module/LoadModule cgi_module/' /usr/local/apache2/conf/httpd.conf
 RUN echo "AddHandler cgi-script .pl" >> /usr/local/apache2/conf/httpd.conf
 RUN sed -i 's/Options Indexes FollowSymLinks/Options Indexes FollowSymLinks ExecCGI/' /usr/local/apache2/conf/httpd.conf
 
-# Copiar archivos
+# COPIAR ARCHIVOS (Asegúrate de que estén en la raíz de tu GitHub)
 COPY ./index.html /usr/local/apache2/htdocs/
+COPY ./swagger.html /usr/local/apache2/htdocs/
 COPY ./cgi-bin/ /usr/local/apache2/cgi-bin/
 
-# PERMISOS DE ESCRITURA (Vital para que guarde)
+# PERMISOS DE ESCRITURA (Para que el botón Guardar funcione)
 RUN echo "[]" > /usr/local/apache2/cgi-bin/datos.json
 RUN chmod -R 777 /usr/local/apache2/cgi-bin/
 RUN chown -R www-data:www-data /usr/local/apache2/cgi-bin/
--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -5,14 +5,16 @@ use CGI;
 use JSON;
 use File::Slurp;
 
+# 1. Configuración inicial
 my $cgi = CGI->new;
 my $metodo = $cgi->request_method();
 
-# RUTA ABSOLUTA PARA RENDER
+# RUTA ABSOLUTA: Clave para que Render no falle al escribir
 my $ruta_db = "/usr/local/apache2/cgi-bin/datos.json";
 my $token_maestro = "admin123";
 my $token_cliente = $cgi->http('HTTP_X_AUTH_TOKEN') || $cgi->http('X-Auth-Token') || "";
 
+# 2. Encabezados para el navegador y Swagger
 print $cgi->header(
     -type => 'application/json',
     -charset => 'utf-8',
@@ -21,40 +23,50 @@ print $cgi->header(
     -access_control_allow_headers => 'Content-Type, X-Auth-Token'
 );
 
+# Salir si es una consulta de pre-vuelo (CORS)
 exit if $metodo eq 'OPTIONS';
 
-# Leer base de datos
+# 3. Leer los datos actuales del archivo JSON
 my $personajes = [];
 if (-e $ruta_db) {
     my $contenido = read_file($ruta_db);
     $personajes = decode_json($contenido || '[]');
 }
 
-# --- ENDPOINTS ---
+# --- ENDPOINTS (Lógica de la API) ---
 
 if ($metodo eq 'GET') {
-    # Parámetros de búsqueda: ?id=1 o ?universo=Dragon Ball
+    # PARÁMETROS: Capturamos id o universo de la URL
     my $id_p = $cgi->param('id');
     my $uni_p = $cgi->param('universo');
 
     my @res = @$personajes;
+
+    # Filtrar si se envió un parámetro
     if ($id_p) { @res = grep { $_->{id} == $id_p } @res; }
     if ($uni_p) { @res = grep { lc($_->{universo}) eq lc($uni_p) } @res; }
 
     print encode_json(\@res);
 }
 elsif ($metodo eq 'POST') {
-    if ($token_cliente ne $token_maestro) { print encode_json({error=>"Token invalido"}); exit; }
-    
+    # Seguridad
+    if ($token_cliente ne $token_maestro) { 
+        print encode_json({ error => "Token invalido" }); 
+        exit; 
+    }
+
+    # Leer el JSON enviado desde el index
     my $json_raw = $cgi->param('POSTDATA') || "{}";
     my $nuevo = decode_json($json_raw);
     
-    # Auto-generar ID
+    # Generar el siguiente ID automáticamente
     $nuevo->{id} = (@$personajes > 0) ? $personajes->[-1]->{id} + 1 : 1;
     
+    # Agregar a la lista y guardar en el archivo
     push @$personajes, $nuevo;
     write_file($ruta_db, {binmode => ':utf8'}, encode_json($personajes));
-    print encode_json({ status => "Guardado", id => $nuevo->{id} });
+    
+    print encode_json({ status => "Guardado con exito", id => $nuevo->{id} });
 }
 elsif ($metodo eq 'PUT') {
     if ($token_cliente ne $token_maestro) { print encode_json({error=>"Token invalido"}); exit; }
@@ -65,22 +77,26 @@ elsif ($metodo eq 'PUT') {
 
     for (my $i = 0; $i < @$personajes; $i++) {
         if ($personajes->[$i]->{id} == $edit->{id}) {
-            $personajes->[$i] = $edit;
-            $exito = 1; last;
+            $personajes->[$i] = $edit; # Reemplazar datos
+            $exito = 1; 
+            last;
         }
     }
+
     if ($exito) {
         write_file($ruta_db, {binmode => ':utf8'}, encode_json($personajes));
         print encode_json({ status => "Actualizado" });
     } else {
-        print encode_json({ error => "ID no encontrado" });
+        print encode_json({ error => "No se encontro el ID" });
     }
 }
 elsif ($metodo eq 'DELETE') {
     if ($token_cliente ne $token_maestro) { print encode_json({error=>"Token invalido"}); exit; }
     
     my $id_b = $cgi->param('id');
-    my @filt = grep { $_->{id} != $id_b } @$personajes;
-    write_file($ruta_db, {binmode => ':utf8'}, encode_json(\@filt));
-    print encode_json({ status => "Eliminado" });
+    # Filtrar la lista para quitar el ID seleccionado
+    my @nueva_lista = grep { $_->{id} != $id_b } @$personajes;
+    
+    write_file($ruta_db, {binmode => ':utf8'}, encode_json(\@nueva_lista));
+    print encode_json({ status => "Eliminado correctamente" });
 }
