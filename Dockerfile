a4016cd733df1aeca9b35e454e33b07523e44494
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-04 09:11:59 -0600

    actu

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,20 @@
-# 1. Usamos una imagen estable de Debian
-FROM debian:bullseye
+FROM httpd:2.4
 
-# 2. Instalamos Apache, Perl y las librerías para manejar JSON y CGI
-RUN apt-get update && \
-    apt-get install -y apache2 perl libcgi-pm-perl libjson-perl && \
-    apt-get clean
+# Instalar Perl y JSON
+RUN apt-get update && apt-get install -y perl libjson-perl libfile-slurp-perl && rm -rf /var/lib/apt/lists/*
 
-# 3. ACTIVAR MÓDULOS CRÍTICOS
-# cgid: Para ejecutar Perl
-# headers: Para permitir PATCH y evitar errores de CORS si pruebas desde fuera
-RUN a2enmod cgid
-RUN a2enmod headers
+# Configurar Apache para CGI
+RUN sed -i 's/#LoadModule cgi_module/LoadModule cgi_module/' /usr/local/apache2/conf/httpd.conf
+RUN echo "AddHandler cgi-script .pl" >> /usr/local/apache2/conf/httpd.conf
+RUN sed -i 's/Options Indexes FollowSymLinks/Options Indexes FollowSymLinks ExecCGI/' /usr/local/apache2/conf/httpd.conf
 
-# 4. CONFIGURAR APACHE PARA PERMITIR TODO
-# Esto evita el error "Not Found" y permite que el PATCH funcione
-RUN echo "<Directory /var/www/html>\n\
-    Options Indexes FollowSymLinks\n\
-    AllowOverride All\n\
-    Require all granted\n\
-    DirectoryIndex index.html\n\
-</Directory>" > /etc/apache2/conf-available/custom-access.conf && \
-    a2enconf custom-access
+# Copiar archivos
+COPY ./index.html /usr/local/apache2/htdocs/
+COPY ./cgi-bin/ /usr/local/apache2/cgi-bin/
 
-# 5. LIMPIAR Y COPIAR ARCHIVOS
-RUN rm -rf /var/www/html/*
+# PERMISOS DE ESCRITURA (Vital para que guarde)
+RUN echo "[]" > /usr/local/apache2/cgi-bin/datos.json
+RUN chmod -R 777 /usr/local/apache2/cgi-bin/
+RUN chown -R www-data:www-data /usr/local/apache2/cgi-bin/
 
-# Copiamos la interfaz y la documentación a la raíz
-COPY index.html /var/www/html/index.html
-COPY swagger.html /var/www/html/swagger.html
-COPY swagger.json /var/www/html/swagger.json
-
-# Copiamos los scripts de Perl y la base de datos a cgi-bin
-COPY ./cgi-bin/ /usr/lib/cgi-bin/
-
-# 6. PERMISOS DE SEGURIDAD Y ESCRITURA
-# Permiso de ejecución para el script
-RUN chmod +x /usr/lib/cgi-bin/api.pl
-# Permiso total a la carpeta cgi-bin para que Perl pueda escribir archivos
-RUN chmod 777 /usr/lib/cgi-bin
-# Permiso de lectura/escritura para la base de datos JSON
-RUN chmod 666 /usr/lib/cgi-bin/datos.json
-
-# 7. EXPONER PUERTO Y LANZAR
 EXPOSE 80
-CMD ["apachectl", "-D", "FOREGROUND"]
--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -3,65 +3,84 @@ use strict;
 use warnings;
 use CGI;
 use JSON;
+use File::Slurp;
 
 my $cgi = CGI->new;
-my $archivo = '/usr/lib/cgi-bin/datos.json';
-
-print $cgi->header('application/json; charset=UTF-8');
-
-sub leer_db {
-    if (!-e $archivo) { return { data => [] }; }
-    open my $fh, '<', $archivo or return { data => [] };
-    my $content = do { local $/; <$fh> };
-    close $fh;
-    return $content ? decode_json($content) : { data => [] };
-}
-
-sub guardar_db {
-    my ($datos) = @_;
-    open my $fh, '>', $archivo;
-    print $fh encode_json($datos);
-    close $fh;
-}
-
-my $db = leer_db();
 my $metodo = $cgi->request_method();
 
+# RUTA ABSOLUTA PARA RENDER
+my $ruta_db = "/usr/local/apache2/cgi-bin/datos.json";
+my $token_maestro = "admin123";
+my $token_cliente = $cgi->http('HTTP_X_AUTH_TOKEN') || $cgi->http('X-Auth-Token') || "";
+
+print $cgi->header(
+    -type => 'application/json',
+    -charset => 'utf-8',
+    -access_control_allow_origin => '*',
+    -access_control_allow_methods => 'GET, POST, PUT, DELETE, OPTIONS',
+    -access_control_allow_headers => 'Content-Type, X-Auth-Token'
+);
+
+exit if $metodo eq 'OPTIONS';
+
+# Leer base de datos
+my $personajes = [];
+if (-e $ruta_db) {
+    my $contenido = read_file($ruta_db);
+    $personajes = decode_json($contenido || '[]');
+}
+
+# --- ENDPOINTS ---
+
 if ($metodo eq 'GET') {
-    my $id = $cgi->param('id');
-    if ($id) {
-        my ($p) = grep { $_->{id} == $id } @{$db->{data}};
-        print encode_json($p || { error => "No existe" });
-    } else {
-        print encode_json($db);
-    }
-} 
+    # Parámetros de búsqueda: ?id=1 o ?universo=Dragon Ball
+    my $id_p = $cgi->param('id');
+    my $uni_p = $cgi->param('universo');
+
+    my @res = @$personajes;
+    if ($id_p) { @res = grep { $_->{id} == $id_p } @res; }
+    if ($uni_p) { @res = grep { lc($_->{universo}) eq lc($uni_p) } @res; }
+
+    print encode_json(\@res);
+}
 elsif ($metodo eq 'POST') {
-    my $nuevo = decode_json($cgi->param('POSTDATA'));
-    my $max_id = 0;
-    foreach my $p (@{$db->{data}}) { $max_id = $p->{id} if $p->{id} > $max_id; }
-    $nuevo->{id} = $max_id + 1;
-    push @{$db->{data}}, $nuevo;
-    guardar_db($db);
-    print encode_json({ status => "Creado", id => $nuevo->{id} });
-} 
-elsif ($metodo eq 'PATCH') {
-    my $update = decode_json($cgi->param('POSTDATA'));
-    foreach my $p (@{$db->{data}}) {
-        if ($p->{id} == $update->{id}) {
-            $p->{nombre} = $update->{nombre} if $update->{nombre};
-            $p->{universo} = $update->{universo} if $update->{universo};
-            $p->{nivel_poder} = $update->{nivel_poder} if $update->{nivel_poder};
-            last;
+    if ($token_cliente ne $token_maestro) { print encode_json({error=>"Token invalido"}); exit; }
+    
+    my $json_raw = $cgi->param('POSTDATA') || "{}";
+    my $nuevo = decode_json($json_raw);
+    
+    # Auto-generar ID
+    $nuevo->{id} = (@$personajes > 0) ? $personajes->[-1]->{id} + 1 : 1;
+    
+    push @$personajes, $nuevo;
+    write_file($ruta_db, {binmode => ':utf8'}, encode_json($personajes));
+    print encode_json({ status => "Guardado", id => $nuevo->{id} });
+}
+elsif ($metodo eq 'PUT') {
+    if ($token_cliente ne $token_maestro) { print encode_json({error=>"Token invalido"}); exit; }
+    
+    my $json_raw = $cgi->param('POSTDATA') || "{}";
+    my $edit = decode_json($json_raw);
+    my $exito = 0;
+
+    for (my $i = 0; $i < @$personajes; $i++) {
+        if ($personajes->[$i]->{id} == $edit->{id}) {
+            $personajes->[$i] = $edit;
+            $exito = 1; last;
         }
     }
-    guardar_db($db);
-    print encode_json({ status => "Ok" });
-} 
+    if ($exito) {
+        write_file($ruta_db, {binmode => ':utf8'}, encode_json($personajes));
+        print encode_json({ status => "Actualizado" });
+    } else {
+        print encode_json({ error => "ID no encontrado" });
+    }
+}
 elsif ($metodo eq 'DELETE') {
-    my $id = $cgi->param('id');
-    my @lista = grep { $_->{id} != $id } @{$db->{data}};
-    $db->{data} = \@lista;
-    guardar_db($db);
+    if ($token_cliente ne $token_maestro) { print encode_json({error=>"Token invalido"}); exit; }
+    
+    my $id_b = $cgi->param('id');
+    my @filt = grep { $_->{id} != $id_b } @$personajes;
+    write_file($ruta_db, {binmode => ':utf8'}, encode_json(\@filt));
     print encode_json({ status => "Eliminado" });
 }
