ea8d419cd3620b250a4aab0d6ba552c6f2dcae75
sjdkell829-gif <sjdkell829@gmail.com>
2026-03-03 19:19:33 -0600

    actu

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,13 @@
 FROM httpd:2.4
-
-# Librerías
 RUN apt-get update && apt-get install -y perl libjson-perl libfile-slurp-perl && rm -rf /var/lib/apt/lists/*
-
-# Configuración CGI
 RUN sed -i 's/#LoadModule cgi_module/LoadModule cgi_module/' /usr/local/apache2/conf/httpd.conf
 RUN echo "AddHandler cgi-script .pl" >> /usr/local/apache2/conf/httpd.conf
 RUN sed -i 's/Options Indexes FollowSymLinks/Options Indexes FollowSymLinks ExecCGI/' /usr/local/apache2/conf/httpd.conf
-
-# Copia
 COPY ./index.html /usr/local/apache2/htdocs/
 COPY ./swagger.html /usr/local/apache2/htdocs/
 COPY ./cgi-bin/ /usr/local/apache2/cgi-bin/
 
-# PERMISOS TOTALES (Solución al error de guardado)
+# PERMISOS CRÍTICOS PARA EL GUARDADO
 RUN echo "[]" > /usr/local/apache2/cgi-bin/datos.json
 RUN chmod -R 777 /usr/local/apache2/cgi-bin/
 RUN chown -R www-data:www-data /usr/local/apache2/cgi-bin/
--- cgi-bin/api.pl
+++ cgi-bin/api.pl
@@ -7,6 +7,8 @@ use File::Slurp;
 
 my $cgi = CGI->new;
 my $metodo = $cgi->request_method();
+
+# RUTA ABSOLUTA PARA RENDER (Evita fallos de guardado)
 my $ruta_db = "/usr/local/apache2/cgi-bin/datos.json";
 my $token_maestro = "admin123";
 my $token_cliente = $cgi->http('HTTP_X_AUTH_TOKEN') || $cgi->http('X-Auth-Token') || "";
@@ -21,33 +23,42 @@ print $cgi->header(
 
 exit if $metodo eq 'OPTIONS';
 
-# Leer Base de Datos
+# Cargar base de datos
 my $personajes = [];
 if (-e $ruta_db) {
     my $contenido = read_file($ruta_db);
     $personajes = decode_json($contenido || '[]');
 }
 
-# Seguridad para métodos que modifican datos
+# Seguridad: Solo GET es libre, los demás requieren token
 if ($metodo ne 'GET' && $token_cliente ne $token_maestro) {
-    print encode_json({ error => "Token invalido" });
+    print encode_json({ error => "Token invalido o ausente" });
     exit;
 }
 
-# --- ENDPOINTS ---
-
+# --- LÓGICA CON PARÁMETROS ---
 if ($metodo eq 'GET') {
     my $id_p = $cgi->param('id');
+    my $uni_p = $cgi->param('universo');
+    my $pow_p = $cgi->param('poder_min');
+
     my @res = @$personajes;
+
     if ($id_p) { @res = grep { $_->{id} == $id_p } @res; }
+    if ($uni_p) { @res = grep { lc($_->{universo}) eq lc($uni_p) } @res; }
+    if ($pow_p) { @res = grep { $_->{nivel_poder} >= $pow_p } @res; }
+
     print encode_json(\@res);
 }
 elsif ($metodo eq 'POST') {
     my $json_raw = $cgi->param('POSTDATA') || "{}";
     my $nuevo = decode_json($json_raw);
+    
+    # Generar ID y Rango
     $nuevo->{id} = (@$personajes > 0) ? $personajes->[-1]->{id} + 1 : 1;
     my $edad = $nuevo->{edad} || 0;
     $nuevo->{rango} = $edad >= 100 ? "Legendario" : ($edad >= 18 ? "Adulto" : "Joven");
+
     push @$personajes, $nuevo;
     write_file($ruta_db, {binmode => ':utf8'}, encode_json($personajes));
     print encode_json({ status => "Creado", id => $nuevo->{id} });
@@ -56,12 +67,12 @@ elsif ($metodo eq 'PUT') {
     my $json_raw = $cgi->param('POSTDATA') || "{}";
     my $edit = decode_json($json_raw);
     my $exito = 0;
+
     for (my $i = 0; $i < @$personajes; $i++) {
         if ($personajes->[$i]->{id} == $edit->{id}) {
             $edit->{rango} = ($edit->{edad} >= 100) ? "Legendario" : (($edit->{edad} >= 18) ? "Adulto" : "Joven");
-            $personajes->[$i] = $edit;
-            $exito = 1;
-            last;
+            $personajes->[ $i ] = $edit;
+            $exito = 1; last;
         }
     }
     if ($exito) {
@@ -75,5 +86,5 @@ elsif ($metodo eq 'DELETE') {
     my $id_b = $cgi->param('id');
     my @filt = grep { $_->{id} != $id_b } @$personajes;
     write_file($ruta_db, {binmode => ':utf8'}, encode_json(\@filt));
-    print encode_json({ status => "Eliminado" });
+    print encode_json({ status => "Eliminado", id => $id_b });
 }
